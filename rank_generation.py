780ebdbe3e9cf8c189bd39095ca4c886c649819e
Samuel Rönnqvist <samuel.ronnqvist@gmail.com>
2020-04-08 22:14:24 +0300

    debug

--- rank_generation.py
+++ rank_generation.py
@@ -1,16 +1,20 @@
 import sys
 
+def print_ranking(candidates):
+    candidates.sort()
+    for _,id,sent in candidates:
+        print("%s\t%s" % (id, sent.strip()))
+
 candidates = []
 for row in sys.stdin:
     if row:
         length, min_length, id, prob, copy_err, repeat_err, sent = row.split('\t')
-        if len(candidates) == 0 or candidates[-1][1] == id:
-            repeat_err = float(repeat_err)
-            if repeat_err < 0.75:
-                repeat_err = 0.0
-            candidates.append(((float(copy_err), float(repeat_err), 1-float(prob)), id, sent))
-        else:
-            candidates.sort()
-            for _,id,sent in candidates:
-                print("%s\t%s" % (id, sent.strip()))
+        repeat_err = float(repeat_err)
+        if repeat_err < 0.75:
+            repeat_err = 0.0
+        if len(candidates) > 0 and candidates[-1][1] != id:
+            print_ranking(candidates)
             candidates = []
+        candidates.append(((float(copy_err), float(repeat_err), 1-float(prob)), id, sent))
+else:
+    print_ranking(candidates)
